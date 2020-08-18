2d5099ac96131ed2cb4991f31c31ac9a3aeda455
HayeGregory <haye.greg@outlook.com>
2020-08-18 14:21:57 +0200

    EX10 - a recuperer (dorian)

---
+++ Exercices/src/Compte.java
@@ -0,0 +1,19 @@
+public class Compte {
+
+    /**
+     * - numero de compte ( string)
+     * - solde (double) - lecture seule
+     * - titulaire - Personne
+     *
+     *
+     * */
+
+    // Attributes
+    private String numeroCompte;
+    private double solde;
+
+    // Getters - Setters
+    // Contructors
+    // Methods
+    // Override
+}
